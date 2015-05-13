71d5b0016d61ad5ce67f9cc4daa50da5f55fda1c
Samuel Clark <sclark0322@gmail.com>
2015-05-12 20:33:59 -0700

    sound buffer, very basic. end of episode 9

--- test/main.cpp
+++ test/main.cpp
@@ -4,8 +4,19 @@
 
 #include <windows.h>
 #include <Xinput.h>
+#include <mmreg.h>
+#include <dsound.h>
 
 #include <stdint.h>
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+typedef float real32;
+typedef double real64;
+
+#define Pi32 3.14159265359f
 
 #include "Logging.h"
 #include "Console.h"
@@ -31,7 +42,7 @@ typedef X_INPUT_SET_STATE(XInputSetStateFunc);
 global XInputSetStateFunc* x_input_set_state = nullptr;
 
 // note: this is loading manually from a dll
-internal void win32_load_xinput()
+internal void win32_load_x_input()
 {
   HMODULE x_input_lib = LoadLibrary(L"xinput1_4.dll");
   if (x_input_lib != nullptr) {
@@ -54,6 +65,147 @@ internal void win32_load_xinput()
   }
 
   LOG(WARNING) << "failed to load xinput";
+}
+
+global LPDIRECTSOUNDBUFFER sound_buffer = nullptr;
+
+internal void win32_init_d_sound(HWND window, int32_t sample_rate, int32_t buffer_size)
+{
+  // todo: use a more recent sound api
+  HMODULE d_sound_lib = LoadLibrary(L"dsound.dll");
+  if (d_sound_lib == nullptr) {
+    LOG(WARNING) << "failed to load dsound";
+    return;
+  }
+  typedef HRESULT WINAPI DirectSoundCreateFunc(LPCGUID, LPDIRECTSOUND*, LPUNKNOWN);
+  DirectSoundCreateFunc* direct_sound_create = (DirectSoundCreateFunc*)GetProcAddress(d_sound_lib, "DirectSoundCreate");
+
+  if (direct_sound_create == nullptr) {
+    LOG(ERROR) << "failed to get proc address of DirectSoundCreate";
+    return;
+  }
+
+  LPDIRECTSOUND direct_sound;
+  if (!SUCCEEDED(direct_sound_create(0, &direct_sound, nullptr))) {
+    LOG(ERROR) << "failed to create direct sound";
+    return;
+  }
+
+  if (!SUCCEEDED(direct_sound->SetCooperativeLevel(window, DSSCL_PRIORITY))) {
+    LOG(ERROR) << "failed to set cooperative level";
+    direct_sound->Release();
+    return;
+  }
+
+  WAVEFORMATEX wave_format = {};
+  wave_format.wFormatTag = WAVE_FORMAT_PCM;
+  wave_format.nChannels = 2;
+  wave_format.nSamplesPerSec = sample_rate;
+  wave_format.wBitsPerSample = 16;
+  wave_format.nBlockAlign = (wave_format.nChannels * wave_format.wBitsPerSample) / 8;
+  wave_format.nAvgBytesPerSec = wave_format.nSamplesPerSec * wave_format.nBlockAlign;
+  
+  // primary buffer is never actually called, this just gets a handle to the sound card and tells it the format that
+  // we'll be passing to it
+  DSBUFFERDESC primary_buffer_desc = {};
+  primary_buffer_desc.dwSize = sizeof(primary_buffer_desc);
+  primary_buffer_desc.dwFlags = DSBCAPS_PRIMARYBUFFER;
+  LPDIRECTSOUNDBUFFER primary_buffer;
+  if (SUCCEEDED(direct_sound->CreateSoundBuffer(&primary_buffer_desc, &primary_buffer, nullptr))) {
+    if (!SUCCEEDED(primary_buffer->SetFormat(&wave_format))) {
+      LOG(ERROR) << "failed to set primary buffer format";
+      primary_buffer->Release();
+      direct_sound->Release();
+      return;
+    }
+  } else {
+    LOG(ERROR) << "failed to create primary buffer";
+    direct_sound->Release();
+    return;
+  }
+
+  DSBUFFERDESC secondary_buffer_desc = {};
+  secondary_buffer_desc.dwSize = sizeof(secondary_buffer_desc);
+  secondary_buffer_desc.dwFlags = 0;
+  secondary_buffer_desc.dwBufferBytes = sample_rate;
+  secondary_buffer_desc.dwBufferBytes = buffer_size;
+  secondary_buffer_desc.lpwfxFormat = &wave_format;
+  if (!SUCCEEDED(direct_sound->CreateSoundBuffer(&secondary_buffer_desc, &sound_buffer, nullptr))) {
+    LOG(ERROR) << "failed to create secondary buffer";
+    primary_buffer->Release();
+    direct_sound->Release();
+    return;
+  } else {
+    LOG(SUCCESS) << "successfully created sound buffer";
+  }
+}
+
+struct win32_sound {
+  int sample_rate;
+  int hz;
+  int period;
+  int volume;
+  int sample_size;
+  uint32_t sample_index;
+};
+
+internal void win32_fill_sound_buffer(win32_sound* sound, DWORD byte_to_lock, DWORD bytes_to_write)
+{
+  VOID* region1;
+  DWORD region1_size;
+  VOID* region2;
+  DWORD region2_size;
+  if (!SUCCEEDED(sound_buffer->Lock(byte_to_lock, bytes_to_write, &region1, &region1_size, &region2, &region2_size, 0))) {
+    return;
+  }
+
+  DWORD region1_sample_count = region1_size / sound->sample_size;
+  int16* sample_out = (int16*)region1;
+  for (DWORD sample_index = 0; sample_index < region1_sample_count; ++sample_index) {
+    real32 t = 2.0f * Pi32 * static_cast<real32>(sound->sample_index++) / static_cast<real32>(sound->period);
+    int16 sample_value = static_cast<int16>(sinf(t) * static_cast<real32>(2000));
+    *(sample_out++) = sample_value;
+    *(sample_out++) = sample_value;
+  }
+
+  DWORD region2_sample_count = region2_size / sound->sample_size;
+  sample_out = (int16*)region2;
+  for (DWORD sample_index = 0; sample_index < region2_sample_count; ++sample_index) {
+    real32 t = 2.0f * Pi32 * static_cast<real32>(sound->sample_index++) / static_cast<real32>(sound->period);
+    int16 sample_value = static_cast<int16>(sinf(t) * static_cast<real32>(2000));
+    *(sample_out++) = sample_value;
+    *(sample_out++) = sample_value;
+  }
+
+  sound_buffer->Unlock(region1, region1_size, region2, region2_size);
+}
+
+internal void win32_play_sound(win32_sound* sound)
+{
+  DWORD play_cursor;
+  DWORD write_cursor;
+  if (!SUCCEEDED(sound_buffer->GetCurrentPosition(&play_cursor, &write_cursor))) {
+    return;
+  }
+  
+  DWORD buffer_size = sound->sample_rate * sound->sample_size;
+
+  DWORD byte_to_lock = (sound->sample_index * sound->sample_size) % buffer_size;
+  DWORD bytes_to_write;
+  if (byte_to_lock == play_cursor) {
+    if (sound->sample_index != 0) {
+      bytes_to_write = 0;
+    } else {
+      bytes_to_write = buffer_size;
+    }
+  } else if (byte_to_lock > play_cursor) {
+    bytes_to_write = buffer_size - byte_to_lock;
+    bytes_to_write += play_cursor;
+  } else {
+    bytes_to_write = play_cursor - byte_to_lock;
+  }
+
+  win32_fill_sound_buffer(sound, byte_to_lock, bytes_to_write);
 }
 
 struct win32OffscreenBuffer {
@@ -84,7 +236,7 @@ struct win32OffscreenBuffer {
     info.bmiHeader.biCompression = BI_RGB;
 
     int buffer_size = size();
-    data = VirtualAlloc(nullptr, buffer_size, MEM_COMMIT, PAGE_READWRITE);
+    data = VirtualAlloc(nullptr, buffer_size, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
     if (data == nullptr) {
       // TODO: THROW EXCEPTION INSTEAD OF LOGGING
       LOG(FATAL) << "could not allocate backbuffer";
@@ -96,8 +248,7 @@ struct win32OffscreenBuffer {
 };
 global win32OffscreenBuffer backbuffer;
 
-internal Dimension
-win32_get_window_dimension(HWND window)
+internal Dimension win32_get_window_dimension(HWND window)
 {
   Dimension result;
 
@@ -149,6 +300,8 @@ internal void win32_process_keyboard(WPARAM w_param, LPARAM l_param)
   bool was_down = (l_param & (1 << 30)) != 0;
   bool is_down = (l_param & (1 << 31)) == 0;
 
+  bool alt_mask = (l_param & (1 << 29)) != 0;
+
   switch(vk_code) {
   case 'W':
   case VK_UP:
@@ -164,6 +317,9 @@ internal void win32_process_keyboard(WPARAM w_param, LPARAM l_param)
     break;
   case VK_ESCAPE:
     if (is_down) running = false;
+    break;
+  case VK_F4:
+    if (alt_mask) running = false;
     break;
   }
 }
@@ -182,41 +338,38 @@ internal void win32_vibrate_controller(DWORD index, int32_t left = 0xFFFF, int32
   x_input_set_state(index, &vibration);
 }
 
-internal void win32_process_gamepad()
+internal void win32_process_gamepad(DWORD controller_index)
 {
   XINPUT_STATE controller_state;
-  for (DWORD controller_index = 0; controller_index < XUSER_MAX_COUNT; ++controller_index)
-  {
-    if (x_input_get_state(controller_index, &controller_state) == ERROR_SUCCESS) {
-      auto& pad = controller_state.Gamepad;
+  if (x_input_get_state(controller_index, &controller_state) == ERROR_SUCCESS) {
+    auto& pad = controller_state.Gamepad;
 
-      bool up = pad.wButtons & XINPUT_GAMEPAD_DPAD_UP;
-      bool down = pad.wButtons & XINPUT_GAMEPAD_DPAD_DOWN;
-      bool left = pad.wButtons & XINPUT_GAMEPAD_DPAD_LEFT;
-      bool right = pad.wButtons & XINPUT_GAMEPAD_DPAD_RIGHT;
-      bool back = pad.wButtons & XINPUT_GAMEPAD_BACK;
-      bool start = pad.wButtons & XINPUT_GAMEPAD_START;
-      bool a = pad.wButtons & XINPUT_GAMEPAD_A;
-      bool b = pad.wButtons & XINPUT_GAMEPAD_B;
-      bool x = pad.wButtons & XINPUT_GAMEPAD_X;
-      bool y = pad.wButtons & XINPUT_GAMEPAD_Y;
-      bool left_shoulder = pad.wButtons & XINPUT_GAMEPAD_LEFT_SHOULDER;
-      bool right_shoulder = pad.wButtons & XINPUT_GAMEPAD_RIGHT_SHOULDER;
+    bool up = pad.wButtons & XINPUT_GAMEPAD_DPAD_UP;
+    bool down = pad.wButtons & XINPUT_GAMEPAD_DPAD_DOWN;
+    bool left = pad.wButtons & XINPUT_GAMEPAD_DPAD_LEFT;
+    bool right = pad.wButtons & XINPUT_GAMEPAD_DPAD_RIGHT;
+    bool back = pad.wButtons & XINPUT_GAMEPAD_BACK;
+    bool start = pad.wButtons & XINPUT_GAMEPAD_START;
+    bool a = pad.wButtons & XINPUT_GAMEPAD_A;
+    bool b = pad.wButtons & XINPUT_GAMEPAD_B;
+    bool x = pad.wButtons & XINPUT_GAMEPAD_X;
+    bool y = pad.wButtons & XINPUT_GAMEPAD_Y;
+    bool left_shoulder = pad.wButtons & XINPUT_GAMEPAD_LEFT_SHOULDER;
+    bool right_shoulder = pad.wButtons & XINPUT_GAMEPAD_RIGHT_SHOULDER;
 
-      int8_t trigger_left = pad.bLeftTrigger;
-      int8_t trigger_right = pad.bRightTrigger;
+    int8_t trigger_left = pad.bLeftTrigger;
+    int8_t trigger_right = pad.bRightTrigger;
 
-      int16_t stick_x = pad.sThumbLX;
-      int16_t stick_y = pad.sThumbLY;
+    int16_t stick_x = pad.sThumbLX;
+    int16_t stick_y = pad.sThumbLY;
 
-      if (up) ++y_off;
-      else if (down) --y_off;
+    if (up) ++y_off;
+    else if (down) --y_off;
 
-      if (left) --x_off;
-      else if (right) ++x_off;
+    if (left) --x_off;
+    else if (right) ++x_off;
           
-      win32_vibrate_controller(controller_index, static_cast<WORD>(trigger_left) * 4, static_cast<WORD>(trigger_right) * 4);
-    }
+    win32_vibrate_controller(controller_index, static_cast<WORD>(trigger_left) * 4, static_cast<WORD>(trigger_right) * 4);
   }
 }
 
@@ -297,7 +450,25 @@ int APIENTRY _tWinMain(HINSTANCE instance,
   }
 
   win32_resize_DIB_section(&backbuffer, 800, 600);
-  win32_load_xinput();
+  win32_load_x_input();
+
+  DWORD sample_rate = 48000;
+  int16 sample_size = sizeof(int16) * 2;
+  DWORD buffer_size = 48000 * sample_size;
+  win32_init_d_sound(window, sample_rate, buffer_size);
+  win32_sound sound = {};
+  
+  if (sound_buffer != nullptr) {
+    sound.sample_rate = sample_rate;
+    sound.sample_size = sample_size;
+    sound.hz = 256;
+    sound.volume = 1000;
+    sound.period = sound.sample_rate / sound.hz;
+    sound.sample_index = 0;
+
+    win32_fill_sound_buffer(&sound, 0, buffer_size);
+    sound_buffer->Play(0, 0, DSBPLAY_LOOPING);
+  }
   
   MSG message;
   running = true;
@@ -313,7 +484,13 @@ int APIENTRY _tWinMain(HINSTANCE instance,
 
     // poll input
     if (x_input_get_state != nullptr) {
-      win32_process_gamepad();
+      for (DWORD controller_index = 0; controller_index < XUSER_MAX_COUNT; ++controller_index) {
+        win32_process_gamepad(controller_index);
+      }
+    }
+
+    if (sound_buffer != nullptr) {
+      win32_play_sound(&sound);
     }
 
     render_something(&backbuffer, x_off, y_off);
